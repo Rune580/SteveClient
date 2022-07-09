da7e2b03736201c63fb395ddf341f33d68349391
Rune580 <12521934+Rune580@users.noreply.github.com>
2022-07-09 13:48:07 -0400

    start of pain

---
+++ SteveClient.Engine/Rendering/Ui/Elements/InputField.cs
@@ -0,0 +1,10 @@
+﻿using OpenTK.Mathematics;
+
+namespace SteveClient.Engine.Rendering.Ui.Elements;
+
+public class InputField : BaseUiElement
+{
+    public InputField(Box2 rect) : base(rect)
+    {
+    }
+}
